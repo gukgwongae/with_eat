1af614ad30dff5e2febccafaa1d269ad5f727a67
mj012345 <dlswls462@gmail.com>
2025-11-16 18:32:14 +0900

    feature : first commit / MY page ui구현

---
+++ lib/my_page.dart
@@ -0,0 +1,147 @@
+//## my page 화면입니다.
+
+import 'package:flutter/material.dart';
+import 'package:with_eat/add_post_page.dart';
+
+class User {
+  String id = "";
+  String nickname = "";
+  String bestfood = "";
+  String description = "";
+  String profileImageUrl = "";
+  DateTime reservedAt = DateTime.now();
+}
+
+class MyPage extends StatefulWidget {
+  MyPage({super.key});
+  final idController = TextEditingController();
+  final nicknameController = TextEditingController();
+  final bestfoodController = TextEditingController();
+  final descriptionController = TextEditingController();
+  @override
+  State<MyPage> createState() => _MyPageState();
+}
+
+//## 유효성 체크 문구
+class _MyPageState extends State<MyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //## 앱바
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_rounded),
+          onPressed: () {
+            Navigator.pop(context); //## 이전 페이지로 이동
+          },
+        ),
+        title: Text("MY"),
+      ),
+      //## 바디
+      body: Container(
+        color: Colors.white,
+        child: Scrollbar(
+          thumbVisibility: false,
+          trackVisibility: false,
+          interactive: true,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+              child: displayView(
+                widget.nicknameController,
+                widget.bestfoodController,
+                widget.idController,
+                widget.descriptionController,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget displayView(
+  TextEditingController nicknameController,
+  TextEditingController bestfoodController,
+  TextEditingController idController,
+  TextEditingController descriptionController,
+) {
+  return Column(
+    children: [
+      titleLabel("프로필 사진", center: true),
+      SizedBox(height: 5),
+      Align(
+        alignment: AlignmentGeometry.center,
+        child: Container(
+          width: 100,
+          height: 100,
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black12),
+            borderRadius: BorderRadius.circular(100),
+          ),
+          child: Align(
+            alignment: Alignment.center,
+            child: Icon(
+              Icons.camera_alt_rounded,
+              color: Colors.black45,
+              size: 24,
+            ),
+          ),
+        ),
+      ),
+      SizedBox(height: 30),
+      titleLabel("닉네임"),
+      SizedBox(height: 5),
+      TextField(
+        controller: nicknameController,
+        maxLength: 30,
+        maxLines: 1,
+        enabled: false,
+        decoration: styleInputDecoration().copyWith(hintText: '고독한미식가'),
+      ),
+      SizedBox(height: 30),
+      titleLabel("최애 음식"),
+      SizedBox(height: 5),
+      TextField(
+        controller: bestfoodController,
+        maxLength: 30,
+        maxLines: 1,
+        enabled: false,
+        decoration: styleInputDecoration().copyWith(hintText: '삼겹살'),
+      ),
+      SizedBox(height: 30),
+      titleLabel("자기소개"),
+      SizedBox(height: 5),
+      TextField(
+        controller: descriptionController,
+        maxLength: 1000,
+        maxLines: 8,
+        enabled: false,
+        decoration: styleInputDecoration().copyWith(hintText: '삼겹살 러버'),
+      ),
+      SizedBox(height: 30),
+    ],
+  );
+}
+
+// ## TextField 스타일 정의
+InputDecoration styleInputDecoration() {
+  return InputDecoration(
+    counterText: '',
+    border: OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.black12, width: 2),
+      borderRadius: BorderRadius.circular(10.0),
+    ),
+  );
+}
+
+// ## 인풋창 라벨 스타일 정의
+Widget titleLabel(String id, {bool center = false}) {
+  return Align(
+    alignment: center ? Alignment.center : Alignment.centerLeft,
+    child: Text(id),
+  );
+}
