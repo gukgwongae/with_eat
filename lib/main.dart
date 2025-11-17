b945c24b9b148b9be87d4012abafbeb605966876
hangyeol <wkdvusdk12@gmail.com>
2025-11-17 23:51:40 +0900

    feat
    naver map 추가

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:with_eat/view/welcome/welcome_page.dart';
 import 'firebase_options.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await FlutterNaverMap().init(clientId: '37wcvy81k4');
   runApp(MyApp());
 }
 
